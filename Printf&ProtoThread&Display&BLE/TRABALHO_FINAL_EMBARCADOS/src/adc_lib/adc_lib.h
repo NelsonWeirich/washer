71e49c0b8fac6a3662b87c3063933b3229ea73d5
NelsonWeirich <nelsonweirichjr@gmail.com>
2018-06-30 11:39:11 -0300

    Add adc library files
    Co-Authored-By: EduardoCulau <38629292+EduardoCulau@users.noreply.github.com>

--- Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/adc_lib/adc_lib.h
+++ Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/adc_lib/adc_lib.h
@@ -14,5 +14,21 @@
 #include "adc_configure.h"
 #include "adc_temp.h"
 
+//Pino do potenciomentor que vai ser o sensor de nivel da agua.
+#define SENSOR_NIVEL_PIN       //Definir o pino para entrar no ADC. (Verificar manula para ver quais pinos o adc tem acesso e a tensão maxima).
+//Dar uma olhada, mas se não me engado era para ser pinos 3 e 4 de cada conection EXT, logo vamos usar a EXT1 ou EXT2, então vai ser PB00 e PB01, ou PA20 e PA11.
+
+//Valores a serem compardos no main com o valor que sai do adc. Tipo nível baixo o adc retorna 500, logo compara com 500.
+#define SENSOR_NIVEL_ZERO     0
+#define SENSOR_NIVEL_BAIXO    500
+#define SENSOR_NIVEL_MEDIO    1500
+#define SENSOR_NIVEL_ALTO     3000
+#define SENSOR_NIVEL_FULL     -1
+
+//Fazer essas funções.
+void initSensor_NivelAgua(int Sensor);   //Inicializar tudo, mede e tals.
+int  valorAtualSensor(int Sensor);      //Aqui dentro tem um if, ela só retorna os defines.
+
+//Se o adc for por pooling valorAtualSensor deve fazer a medição, se for por DMA, ela deve só ser a variave(memoria) compartilhada.
 
 #endif /* ADC_LIB_H_ */
--- Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/main.c
+++ Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/main.c
@@ -30,12 +30,15 @@ DATA: 25/06/2018
 typedef enum ESTADOS{
 	PEGANDO_DADOS = 0,
 	EXECUTANDO,
+	TAMPA_ABERTA,
 	ESPERA
 } ESTADO_t;
 
 ESTADO_t ESTADO = PEGANDO_DADOS;
 
 typedef enum subEstado{
+	ENXER,
+	ESVAZIAR,
 	BATER,
 	MOLHO,
 	ENXAGUAR,
@@ -48,12 +51,13 @@ EXECUTANDO_t EXECUTION = FAZENDO_NADA;
 
 //Vars
 typedef enum NIVEL__da_AGUA{
-	BAIXO = 0,
-	MEDIO,
-	ALTO,
+	BAIXO = SENSOR_NIVEL_BAIXO,
+	MEDIO = SENSOR_NIVEL_MEDIO,
+	ALTO  = SENSOR_NIVEL_ALTO,
 } NIVEL_t;
 
-NIVEL_t NIVEL_AGUA = ALTO;
+NIVEL_t NIVEL_AGUA   = BAIXO;
+int     NIVEL_SENSOR = SENSOR_NIVEL_ZERO;
 
 typedef enum MODO_EXECUTAR{
 	NORMAL = 0,
@@ -89,75 +93,19 @@ struct timer { int start, interval; };
 //Tempo de espera da protothread
 //#define TIMEOUT 10
 
-//Liga o motor quando for o momento liga-lo.
-PT_THREAD(pt_ligaMotor(struct pt *pt))
-{
-	
-	//Inicia a protothread.
-	PT_BEGIN(pt);
-	
-	//Espera algo.
-	PT_WAIT_UNTIL(pt, 1);
-	
-	//Rerorna para da protothread.
-	PT_YIELD(pt);
-	
-	//Fim da protothread.
-	PT_END(pt);
-}
-
-//Liga a bomba quando for o momento liga-lo.
-PT_THREAD(pt_ligaBomba(struct pt *pt))
-{
-	//Inicia a protothread.
-	PT_BEGIN(pt);
-	
-	//Espera algo.
-	PT_WAIT_UNTIL(pt, 1);
-	
-	//Rerorna para da protothread.
-	PT_YIELD(pt);
-	
-	//Fim da protothread.
-	PT_END(pt);
-}
-
-//Liga a bomba quando for o momento liga-lo.
-PT_THREAD(pt_liberaValvula(struct pt *pt))
-{
-	//Inicia a protothread.
-	PT_BEGIN(pt);
-	
-	//Espera algo.
-	PT_WAIT_UNTIL(pt, 1);
-	
-	//Rerorna para da protothread.
-	PT_YIELD(pt);
-	
-	//Fim da protothread.
-	PT_END(pt);
-}
-
-//Liga o secador quando for o momento liga-lo.
-PT_THREAD(pt_ligaSecador(struct pt *pt))
-{
-	//Inicia a protothread.
-	PT_BEGIN(pt);
-	
-	//Espera algo.
-	PT_WAIT_UNTIL(pt, 1);
-	
-	//Rerorna para da protothread.
-	PT_YIELD(pt);
-	
-	//Fim da protothread.
-	PT_END(pt);
-}	
-
 //Btn usados para controlar o menu.
 #define bt1 BUTTON_1_PIN
 #define bt2 BUTTON_2_PIN
-//LED usado para indicar se tá executando e se (COLOCAR OUTRA COISA);
+
+//Btn usado para pegar se a tampa esta aberta.
+#define bt_tampa BTN_TAMPA_PIN
+//Leds usados para indicar que os componentes forma ligados.
+#define l_motor   LED_MOTOR_PIN
+#define l_secador LED_SECADOR_PIN
+#define l_valvula LED_VALVULA_PIN
+#define l_bomba   LED_BOMBA_PIN   
+
+//LED usado para indicar se tá executando e se (COLOCAR OUTRA COISA); Só esta sendo usado para testes.
 #define l1  LED_1_PIN
 #define l3  LED_3_PIN
 
@@ -196,7 +144,7 @@ PT_THREAD(pt_gerenciaDisplay(struct pt *pt))
 				ESTADO = ESPERA;
 				printString("---PAUSADO---", 20, 10);
 				printString("CONTINUAR      PARAR", 0, 25);
-			}
+			}else
 			//Parar
 			if(isBTN_DOWN(bt2)){
 				ESTADO = PEGANDO_DADOS;
@@ -210,8 +158,16 @@ PT_THREAD(pt_gerenciaDisplay(struct pt *pt))
 				ESTADO = EXECUTANDO;
 				printString("EXECUTANDO:", 0, 10);
 				printString("PAUSAR         PARAR", 0, 25);
-			}
+			}else
 			
+			//Parar
+			if(isBTN_DOWN(bt2)){
+				ESTADO = PEGANDO_DADOS;
+				clearDisplay(); mostraMenuDisplay(&spinners);
+			}
+		}else
+		
+		if(ESTADO == TAMPA_ABERTA){
 			//Parar
 			if(isBTN_DOWN(bt2)){
 				ESTADO = PEGANDO_DADOS;
@@ -252,8 +208,7 @@ PT_THREAD(pt_pegaDados(struct pt *pt))
 			}	
 		}
 		
-		
-		//Testes
+		//Testes, dá pra tirar isso.
 		if(MODO == PESADO){
 			LED_On(l1);
 		}else{
@@ -265,11 +220,90 @@ PT_THREAD(pt_pegaDados(struct pt *pt))
 			LED_Off(l3);	
 		}
 		
-		//Código final
-	
+		//Pegar se a tampa está aberta.
+		if(ESTADO == EXECUTANDO && isBTN_UP(bt_tampa) ){
+			ESTADO = TAMPA_ABERTA;
+			printString("---TAMPA ABERTA---", 10, 10);
+			printString("------         PARAR", 0, 25);
+		}
+		
+		if(ESTADO == TAMPA_ABERTA && isBTN_DOWN(bt_tampa)){
+			ESTADO = EXECUTANDO;
+			printString("EXECUTANDO:", 0, 10);
+			printString("PAUSAR         PARAR", 0, 25);
+		}
+		
+		//Fazer logica com os subestados como o batendo, centrifugando, secando e tals.
+		
 		//Rerorna para da protothread.
 		PT_YIELD(pt);
 	}
+	//Fim da protothread.
+	PT_END(pt);
+}
+
+//Controla a execução do sistema. Exemplo. Agora tem que enxer, agora bater, agroa centrifugar.
+PT_THREAD(pt_contorlaExecution(struct pt *pt))
+{
+	static int alreadyPrint;
+	//Inicia a protothread.
+	PT_BEGIN(pt);
+	
+	//Sempre pegar os dados e os processas.
+	while(1){
+		
+		//Fazer logica com os subestados como o batendo, centrifugando, secando e tals.
+		
+		//Rerorna para da protothread.
+		PT_YIELD(pt);
+	}
+	//Fim da protothread.
+	PT_END(pt);
+}
+
+//Liga os componentes de acordo com o que tem que acontecer no momento.
+PT_THREAD(pt_controlaComponentes(struct pt *pt))
+{
+	//Inicia a protothread.
+	PT_BEGIN(pt);
+	
+	while(1){
+		//***MOTOR***
+		//Estado é executnado, logo verifica se é para o motor executar.
+		if(ESTADO == EXECUTANDO && ( EXECUTION == BATER || EXECUTION == CENTRIFUGAR || EXECUTION == SECANDO )){
+			LED_On(l_motor);
+		}else{
+			LED_Off(l_motor);
+		}
+		
+		//***VALVULA***
+		//Estado é executnado, logo verifica se é para colocar agua.
+		if(ESTADO == EXECUTANDO && ( EXECUTION == ENXER) ){
+			LED_On(l_bomba);
+		}else{
+			LED_Off(l_bomba);
+		}
+		
+		//***BOMBA***
+		//Estado é executnado, logo verifica se é para o bomba retirar agua.
+		if(ESTADO == EXECUTANDO && ( EXECUTION == CENTRIFUGAR || EXECUTION == ESVAZIAR)){
+			LED_On(l_bomba);
+		}else{
+			LED_Off(l_bomba);
+		}
+		
+		//***SECAR***
+		//Estado é executnado, liga quando tiver que secar.
+		if(ESTADO == EXECUTANDO && ( EXECUTION == SECANDO) ){
+			LED_On(l_secador);
+		}else{
+			LED_Off(l_secador);
+		}
+		
+		//Rerorna para da protothread.
+		PT_YIELD(pt);
+	}
+	
 	//Fim da protothread.
 	PT_END(pt);
 }
@@ -286,6 +320,7 @@ int main(void)
 	init_OLED_btnLed();
 	
 	//Configura os btns e os leds externos (lateral)
+	init_external_btnLed();
 
 	//Configura
 	configure_usart();
@@ -299,8 +334,8 @@ int main(void)
 	printf("START....................\n\n");		
 	
 	//Cria a pThread. E Inicializa.
-    struct pt pt_gD, pt_pD, pt_lV, pt_lM, pt_lB, pt_lS;
-	PT_INIT(&pt_gD); PT_INIT(&pt_pD); PT_INIT(&pt_lV); PT_INIT(&pt_lM); PT_INIT(&pt_lB); PT_INIT(&pt_lS);
+    struct pt pt_gD, pt_pD, pt_cC, pt_cE;
+	PT_INIT(&pt_gD); PT_INIT(&pt_pD); PT_INIT(&pt_cC); PT_INIT(&pt_cE);
 	
 	
 	//Fica executado para sempre as protoThreads.
@@ -308,12 +343,8 @@ int main(void)
 		//Fica trocando entre as protothreads.
 		pt_gerenciaDisplay(&pt_gD);
 		pt_pegaDados(&pt_pD);
-		pt_liberaValvula(&pt_lV);
-		pt_ligaMotor(&pt_lM);
-		pt_ligaBomba(&pt_lB);
-		pt_ligaSecador(&pt_lS);
-		//Gasta tempo para cada vetor.
-		//for(int wt = 0; wt<1500;wt++){}
+		pt_controlaComponentes(&pt_cC);
+		pt_contorlaExecution(&pt_cE);
 	}
 	
 }
