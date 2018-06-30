6e981b07ea3ff13ad8143ddc458e8a37bb32e75e
NelsonWeirich <nelsonweirichjr@gmail.com>
2018-06-30 11:44:48 -0300

    Update header file in adc library
    Co-Authored-By: EduardoCulau <38629292+EduardoCulau@users.noreply.github.com>

--- Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/adc_lib/adc_lib.h
+++ Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/adc_lib/adc_lib.h
@@ -11,11 +11,12 @@
 
 
 //Só inclui todas as libs.
+#include <asf.h>
 #include "adc_configure.h"
 #include "adc_temp.h"
 
 //Pino do potenciomentor que vai ser o sensor de nivel da agua.
-#define SENSOR_NIVEL_PIN       //Definir o pino para entrar no ADC. (Verificar manula para ver quais pinos o adc tem acesso e a tensão maxima).
+#define SENSOR_NIVEL_PIN      PIN_PB00 //Definir o pino para entrar no ADC. (Verificar manula para ver quais pinos o adc tem acesso e a tensão maxima).
 //Dar uma olhada, mas se não me engado era para ser pinos 3 e 4 de cada conection EXT, logo vamos usar a EXT1 ou EXT2, então vai ser PB00 e PB01, ou PA20 e PA11.
 
 //Valores a serem compardos no main com o valor que sai do adc. Tipo nível baixo o adc retorna 500, logo compara com 500.
@@ -26,7 +27,7 @@
 #define SENSOR_NIVEL_FULL     -1
 
 //Fazer essas funções.
-void initSensor_NivelAgua(int Sensor);   //Inicializar tudo, mede e tals.
+void initSensor_NivelAgua(void);   //Inicializar tudo, mede e tals.
 int  valorAtualSensor(int Sensor);      //Aqui dentro tem um if, ela só retorna os defines.
 
 //Se o adc for por pooling valorAtualSensor deve fazer a medição, se for por DMA, ela deve só ser a variave(memoria) compartilhada.
