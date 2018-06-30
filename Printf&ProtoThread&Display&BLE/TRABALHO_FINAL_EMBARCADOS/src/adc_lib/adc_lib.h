308d9c1ebe9b0b69a46be4fd1a9498feb106e971
NelsonWeirich <31675487+NelsonWeirich@users.noreply.github.com>
2018-06-30 18:57:10 -0300

    Nothing

--- Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/adc_lib/adc_lib.h
+++ Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/adc_lib/adc_lib.h
@@ -3,35 +3,36 @@
  *
  * Created: 25/06/2018 16:45:19
  *  Author: Eduardo Culau
- */ 
+ */
 
 
 #ifndef ADC_LIB_H_
 #define ADC_LIB_H_
 
 
-//Só inclui todas as libs.
+//SÃ³ inclui todas as libs.
 #include <asf.h>
 #include "adc_configure.h"
 #include "adc_temp.h"
 
-//TODO : Definir pino do adc. Verificar se os modulos estão devidos corretos no asf wizzard.
+//TODO : Definir pino do adc. Verificar se os modulos estï¿½o devidos corretos
+//no asf wizzard.
 //Pino do potenciomentor que vai ser o sensor de nivel da agua.
-#define SENSOR_NIVEL_PIN      PIN_PB00 //Definir o pino para entrar no ADC. (Verificar manula para ver quais pinos o adc tem acesso e a tensão maxima).
-//Dar uma olhada, mas se não me engado era para ser pinos 3 e 4 de cada conection EXT, logo vamos usar a EXT1 ou EXT2, então vai ser PB00 e PB01, ou PA20 e PA11.
+#define SENSOR_NIVEL_PIN      PIN_PB00 //Definir o pino para entrar no ADC. (Verificar manula para ver quais pinos o adc tem acesso e a tensï¿½o maxima).
+//Dar uma olhada, mas se nï¿½o me engado era para ser pinos 3 e 4 de cada conection EXT, logo vamos usar a EXT1 ou EXT2, entï¿½o vai ser PB00 e PB01, ou PA20 e PA11.
 
 //TODO :Definir valores que devem vir do pot.
-//Valores a serem compardos no main com o valor que sai do adc. Tipo nível baixo o adc retorna 500, logo compara com 500.
+//Valores a serem compardos no main com o valor que sai do adc. Tipo nï¿½vel baixo o adc retorna 500, logo compara com 500.
 #define SENSOR_NIVEL_ZERO     0
 #define SENSOR_NIVEL_BAIXO    500
 #define SENSOR_NIVEL_MEDIO    1500
 #define SENSOR_NIVEL_ALTO     3000
 #define SENSOR_NIVEL_FULL     -1
 
-//Fazer essas funções. //TODO : Fazer funções abaixo.
+//TODO : Fazer funï¿½ï¿½es abaixo.
 void initSensor_NivelAgua(void);   //Inicializar tudo, mede e tals.
-int  valorAtualSensor(int Sensor);      //Aqui dentro tem um if, ela só retorna os defines.
+int  valorAtualSensor(int Sensor);      //Aqui dentro tem um if, ela sï¿½ retorna os defines.
 
-//Se o adc for por pooling valorAtualSensor deve fazer a medição, se for por DMA, ela deve só ser a variave(memoria) compartilhada.
+//Se o adc for por pooling valorAtualSensor deve fazer a medido, se for por DMA, ela deve sï¿½ ser a variave(memoria) compartilhada.
 
 #endif /* ADC_LIB_H_ */
