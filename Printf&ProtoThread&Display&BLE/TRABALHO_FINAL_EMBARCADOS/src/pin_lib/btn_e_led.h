80aa218bee57f878579f8f5c662b821fd76446fc
NelsonWeirich <31675487+NelsonWeirich@users.noreply.github.com>
2018-07-01 09:11:41 -0300

    Update pins LED and button

--- Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/pin_lib/btn_e_led.h
+++ Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/pin_lib/btn_e_led.h
@@ -3,7 +3,7 @@
  *
  * Created: 25/06/2018 17:53:21
  *  Author: Eduardo Culau
- */ 
+ */
 
 
 #ifndef BTN_E_LED_H_
@@ -12,18 +12,18 @@
 //OLED1_BTNS_LEDS ==> OLED conectado na parte de baixo.
 #define BUTTON_1_PIN           PIN_PA28
 #define BUTTON_2_PIN           PIN_PA02
-#define BUTTON_3_PIN           PIN_PA03  //Não funciona
+#define BUTTON_3_PIN           PIN_PA03  //Nï¿½o funciona
 #define LED_1_PIN              PIN_PA12
-#define LED_2_PIN              PIN_PA13	 //Não funciona.
+#define LED_2_PIN              PIN_PA13	 //Nï¿½o funciona.
 #define LED_3_PIN              PIN_PA15
 
-//TODO :Definir os pinos corretos dos btns e dos led conectados na placa. Definir os pinos deles.
+//CHANGED: Definido os pinos corretos dos btns e dos led conectados na placa.
 //Btns e LEDs externos. Tudos vai ligado em uma porta+ e no gnd-. Os btn ficam em pull-up na porta.
-#define BTN_TAMPA_PIN		  PIN_PB12 //Definir pino do btn de pressão. Se der tempo fazer uma interrupção para ele, se não der é só ler dele.
-#define LED_MOTOR_PIN		  PIN_PB13 //Definir o pino do led amarelo, indicando que é o motor.
-#define LED_SECADOR_PIN       PIN_PB13 //Definir pino do vermelho para o secador. 
-#define LED_VALVULA_PIN		  PIN_PB13 //Definir led transparente(azul) para ser a valvula (entrar agua).
-#define LED_BOMBA_PIN         PIN_PB13 //Definir led verde para ser a bomba (tira agua).
+#define BTN_TAMPA_PIN		  PIN_PB06 //Se der tempo fazer uma interrupï¿½ï¿½o para ele, se nï¿½o der ï¿½ sï¿½ ler dele.
+#define LED_MOTOR_PIN		  PIN_PB07 //Definir o pino do led amarelo, indicando que ï¿½ o motor.
+#define LED_SECADOR_PIN   PIN_PB05 //Definir pino do vermelho para o secador.
+#define LED_VALVULA_PIN		PIN_PA20 //Definir led transparente(azul) para ser a valvula (entrar agua).
+#define LED_BOMBA_PIN     PIN_PA21 //Definir led verde para ser a bomba (tira agua).
 
 //Inicializa todos os btns e os leds.
 void init_OLED_btnLed(void);
