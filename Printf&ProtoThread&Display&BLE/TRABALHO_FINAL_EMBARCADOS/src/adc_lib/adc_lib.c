f202d59e639107b6597fb72a2869661400aee84b
NelsonWeirich <31675487+NelsonWeirich@users.noreply.github.com>
2018-07-02 09:19:19 -0300

    Update final Sensor potentiometer not complete

--- Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/adc_lib/adc_lib.c
+++ Printf&ProtoThread&Display&BLE/TRABALHO_FINAL_EMBARCADOS/src/adc_lib/adc_lib.c
@@ -17,55 +17,30 @@
 /*! Store ADC driver state */
 struct adc_module adc_instance;
 
-/*! Store ADC samples in buffer asynchronously */
-#define ADC_SAMPLES 128
-uint16_t adc_result_buffer[ADC_SAMPLES];
-
-volatile bool adc_read_done = false;
-
-/*! Function callback of the ADC */
-void adc_complete_callback (struct adc_module *const module) {
-	adc_read_done = true;
-}
-
-void configure_adc(void) {
-	// Configuration struct
-  struct adc_config config_adc;
-	// Initialize the ADC configuration struct
-  adc_get_config_defaults(&config_adc);
-	// Change the ADC module configuration to suilt the application
-#if (!SAML21) && (!SAML22) && (!SAMC21)
-  config_adc.gain_factor = ADC_GAIN_FACTOR_DIV2;
-#endif
-  config_adc.clock_prescaler = ADC_CLOCK_PRESCALER_DIV8;
-  config_adc.reference = ADC_REFERENCE_INTVCC1;
-#if (SAMC21)
-  config_adc.positive_input = ADC_POSITIVE_INPUT_PIN5;
-#else
-  config_adc.positive_input = ADC_POSITIVE_INPUT_PIN6;
-#endif
-  config_adc.resolution = ADC_RESOLUTION_12BIT;
-  // Set ADC configurations
-#if (SAMC21)
-  adc_init(&adc_instance, ADC1, &config_adc);
-#else
-  adc_init(&adc_instance, ADC, &config_adc);
-#endif
-  // Enable the ADC module
-  adc_enable(&adc_instance);
-}
-
-/*! Register and enable the ADC Read Buffer Complete callback handler */
-void configure_adc_callbacks(void) {
-	adc_register_callback(&adc_instance, adc_complete_callback, ADC_CALLBACK_READ_BUFFER);
-  adc_enable_callback(&adc_instance, ADC_CALLBACK_READ_BUFFER);
-}
-
 //CHANGED
 /*! Inicializa o sensor */
 void initSensor_NivelAgua(void){
-	configure_adc();
-	configure_adc_callbacks();
+  // Configuration struct
+  struct adc_config config_adc;
+
+  // Initialize the ADC configuration struct
+  adc_get_config_defaults(&config_adc);
+
+  // config_adc.clock_source    = GCLK_GENERATOR_1;
+  // config_adc.clock_prescaler = ADC_CLOCK_PRESCALER_DIV16;
+  // config_adc.reference       = ADC_REFERENCE_INT1V;
+  config_adc.positive_input  = ADC_POSITIVE_INPUT_PIN8; //SENSOR_NIVEL_PIN;
+  config_adc.negative_input  = ADC_NEGATIVE_INPUT_GND;
+  config_adc.sample_length    = 1;
+
+  system_voltage_reference_enable(ADC_REFERENCE_INT1V);
+
+  // Set ADC configurations defaults
+  adc_init(&adc_instance, ADC, &config_adc);
+
+  // ADC->AVGCTRL.reg = ADC_AVGCTRL_ADJRES(2) | ADC_AVGCTRL_SAMPLENUM_4;
+  // Enable the ADC module
+  adc_enable(&adc_instance);
 }
 
 /*! \brief Retorna o valor convertido do ADC
@@ -73,43 +48,48 @@ void initSensor_NivelAgua(void){
  *  \return valor lido do sensor e convertido para um valor entre 0-2047
  */
 int  valorAtualSensor(int Sensor){
-	//Le do adc.
-	// static int count = 0, count2 = 0;
-	// Start an asynchronous ADC conversion
-	adc_read_buffer_job(&adc_instance, adc_result_buffer, ADC_SAMPLES);
+	uint16_t valor_adc;
 
-  // Wait until the asynchronous conversion is complete
-	while (adc_read_done == false) {}
+	//Start conversion
+	adc_start_conversion(&adc_instance);
 
-	if(zero <= valor_adc < 128 ){
-		return SENSOR_NIVEL_ZERO /*!< 0 */
+  // Wait until the conversion is complete
+	do {
+
+	}	while (adc_read(&adc_instance, &valor_adc) == STATUS_BUSY);
+
+  int count = 0, count2 = 0;
+
+  if(count2 > 250){
+  	if(count > 600){
+  		count = 0;
+  	}else{
+  		count++;
+  	}
+  	count2 = 0;
+  }else{
+  	count2++;
+  }
+  return count;
+////////////////////////////////////////
+	if(0 <= valor_adc < 128 ){
+		return SENSOR_NIVEL_ZERO; /*!< 0 */
 	}else
 
 	if(128 <= valor_adc < 700){
-		return SENSOR_NIVEL_BAIXO /*!< 512 */
+		return SENSOR_NIVEL_BAIXO; /*!< 512 */
 	}else
 
 	if(700 <= valor_adc < 1128){
-		return SENSOR_NIVEL_MEDIO /*!< 1023 */
+		return SENSOR_NIVEL_MEDIO; /*!< 1023 */
 	}else
 
 	if(1128 <= valor_adc < 1700){
-		return SENSOR_NIVEL_ALTO /*!< 1540 */
+		return SENSOR_NIVEL_ALTO; /*!< 1540 */
 	}else
 
 	if(1700 <= valor_adc){
-		return SENSOR_NIVEL_FULL /*!< 2047 */
+		return SENSOR_NIVEL_FULL; /*!< 2047 */
 	}
 
-	// if(count2 > 250){
-	// 	if(count > 600){
-	// 		count = 0;
-	// 	}else{
-	// 		count++;
-	// 	}
-	// 	count2 = 0;
-	// }else{
-	// 	count2++;
-	// }
-	// return count;
 }
